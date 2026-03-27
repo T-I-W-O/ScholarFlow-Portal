4cbbc1b9893a28017ef9947d03e05637ca5008c5
T.I.W.O <tiwoadex@gmail.com>
2026-03-27 20:03:26 +0100

    Update urls.py

--- NEPO/urls.py
+++ NEPO/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('success/', views.success, name='success_page'),
     path('oversee/', views.oversee, name='admin'),
     path('update-deadline/', views.update_deadline, name='update_deadline'),
+path('api/universities/', views.get_universities),
     path('student-api/', views.student_api, name='student_api'),
 ]
